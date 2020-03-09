a9dcb945dbb73e2a040db228b79de54236ffcad4
Ty Bandachit <tbandachit@gmail.com>
2020-03-09 22:21:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,50 @@
 # My Code here....
+
+def map_to_negativize(array)
+  newarray = []
+  counter = 0 
+  while counter < array.length do
+    newarray[counter] = (array[counter] * -1)
+    counter += 1
+  end
+  return newarray
+end
+
+def map_to_no_change(array)
+  return array
+end
+
+def map_to_double(array)
+  newarray = []
+  counter = 0
+  while counter < array.length do
+    newarray[counter] = (array[counter] * 2)
+    counter += 1
+  end
+  return newarray
+end
+
+def map_to_square(array)
+  newarray = []
+  counter = 0 
+  while counter < array.length do
+    newarray[counter] = array[counter] * array[counter]
+    counter += 1
+  end 
+  return newarray
+end
+
+def reduce_to_total(array)
+  total = []
+  counter = 0 
+  while counter < array.length do
+  total[0] = total + array[counter]
+  counter += 1
+  end
+return total
+end
+
+
+def reduce 
+  
+end
